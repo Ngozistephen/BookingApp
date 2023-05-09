6fb96b5978f1c2ec498122886df7a536ab422607
Ngozi Stephen <ngozi.stephen99@gmail.com>
2023-05-09 05:40:49 +0100

    Roles and Permission structure of Owner with Spatie Permission Package

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,24 +2,16 @@
 
 namespace App\Models;
 
-use App\Models\Permission;
-use Illuminate\Database\Eloquent\Model;
+
+
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Role extends Model
+class Role extends \Spatie\Permission\Models\Role
 {
     use HasFactory;
-
-    protected $fillable = ['name'];
-    
+ 
     const ROLE_ADMINISTRATOR = 1;
     const ROLE_OWNER = 2;
     const ROLE_USER = 3;
-
-
-    
-    public function permissions()
-    {
-        return $this->belongsToMany(Permission::class);
-    }
 }
